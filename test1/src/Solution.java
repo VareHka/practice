7b2ffebc0f7646f48e719778745a9716923d6aae
VareHka <uuuilia@mail.ru>
2018-11-14 19:03:31 +0700

    Laba2 Dardaev

--- test1/src/Solution.java
+++ test1/src/Solution.java
@@ -1,27 +1,37 @@
 import java.util.*;
+import java.lang.*;
 
-public class Solution {
+public class Solution<MyObject> {
 	/*
 	 *  Дано n - число уровней. Построить треугольник из символов #.
 	 *  Пример для n = 4:
-	 * 	  #
-	 *	 ##
+	 *	   #
+	 *	  ##
 	 *  ###
 	 * ####
 	 */
 
-	static void func1(int n) {
-		int k = n;
-		String o = "";
+	protected static class Wic
+	{
+		private int n;
+		public void SetN(int a){
+			n=a;
 
-		for (int i = 1; i <= n; i++) {
-			k = k - 1;
-			o = o + "#";
-			for (int j = 1; j <= k; j++) {
-				System.out.print(" ");
+		}
+
+		public void func1(){
+			int k = n;
+			String o = "";
+
+			for (int i = 1; i <= n; i++) {
+				k = k - 1;
+				o = o + "#";
+				for (int j = 1; j <= k; j++) {
+					System.out.print(" ");
+				}
+				System.out.print(o);
+				System.out.println();
 			}
-			System.out.print(o);
-			System.out.println();
 		}
 	}
 
@@ -30,7 +40,8 @@ public class Solution {
 	public static void main(String[] args) {
 		int n = scanner.nextInt();
 		scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-
-		func1(n);
+		Wic MyObject= new Wic() ;
+		MyObject.SetN(n);
+		MyObject.func1();
 	}
 }
